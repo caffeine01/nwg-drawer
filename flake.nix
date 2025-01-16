9441bf7445e4cb96968d9d9451151e8315814433
caffeine01 <caffeine@caffeine.moe>
2025-01-16 22:42:20 +0000

    the nixpkgs package was there the whole time

--- flake.nix
+++ flake.nix
@@ -1,98 +1,75 @@
 {
   description = "Development environment and package for nwg-drawer";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     systems.url = "github:nix-systems/default-linux";
   };
-
-  outputs = { self, nixpkgs, systems, ... }@inputs: let
+  outputs = { self, nixpkgs, systems }: let
     inherit (nixpkgs) lib;
-
     gitCommitHash = self.rev or "dirty";
-
-    eachSystem = lib.genAttrs (import systems);
-    pkgsFor = eachSystem (system:
-      import nixpkgs {
-        system = system;
-        overlays = [];
-      });
+    eachSystem = nixpkgs.lib.genAttrs (import systems);
+  in {
+    packages = eachSystem (system: let
+    pkgs = nixpkgs.legacyPackages.${system};
     in {
-      packages = eachSystem (system: {
-        default = pkgsFor.${system}.buildGoModule {
-          pname = "nwg-drawer";
-          version = gitCommitHash;
-
-          vendorHash = "sha256-Q+2CXpPvfy6QYjP+AZeJL/l00/Z+C56c+FfGcyIdQ4M=";
-
-          src = ./.;
-
-          allowGoReference = true;
-
-        nativeBuildInputs = with pkgsFor.${system}; [
-          pkg-config
+      default = pkgs.buildGoModule {
+        pname = "nwg-drawer";
+        version = gitCommitHash;
+        src = ./.;
+        
+        vendorHash = "sha256-Q+2CXpPvfy6QYjP+AZeJL/l00/Z+C56c+FfGcyIdQ4M=";
+        
+        nativeBuildInputs = with pkgs; [
           gobject-introspection
-          makeWrapper
+          pkg-config
+          wrapGAppsHook3
         ];
 
-        buildInputs = with pkgsFor.${system}; [
-          gtk3
+        buildInputs = with pkgs; [
+          cairo
           gtk-layer-shell
-          librsvg
+          gtk3
         ];
 
-        preBuild = ''
-          export PKG_CONFIG_PATH="${pkgsFor.${system}.pkg-config}/lib/pkgconfig:$PKG_CONFIG_PATH"
-          export LD_LIBRARY_PATH="${pkgsFor.${system}.gtk3}/lib:${pkgsFor.${system}.gtk-layer-shell}/lib:$LD_LIBRARY_PATH"
-          export GI_TYPELIB_PATH="${pkgsFor.${system}.gtk3}/lib/girepository-1.0:${pkgsFor.${system}.gtk-layer-shell}/lib/girepository-1.0:$GI_TYPELIB_PATH"
-          export GOPATH="$PWD/.go"
-          export PATH="$GOPATH/bin:$PATH"
-          mkdir -p "$GOPATH"
+        doCheck = false;
+
+        preInstall = ''
+          mkdir -p $out/share/nwg-drawer
+          cp -r desktop-directories drawer.css $out/share/nwg-drawer
         '';
 
-        postInstall = ''
-          wrapProgram $out/bin/nwg-drawer \
-            --prefix LD_LIBRARY_PATH : "${pkgsFor.${system}.gtk3}/lib:${pkgsFor.${system}.gtk-layer-shell}/lib"
+        preFixup = ''
+          # make xdg-open overrideable at runtime
+          gappsWrapperArgs+=(
+           --suffix PATH : ${pkgs.xdg-utils}/bin
+           --prefix XDG_DATA_DIRS : $out/share
+          )
         '';
 
-          meta = with lib; {
-            description = "Application drawer for wlroots-based Wayland compositors";
-            license = licenses.mit;
-            platforms = platforms.linux;
-          };
+        meta = with lib; {
+          description = "Application drawer for wlroots-based Wayland compositors";
+          homepage = "https://github.com/nwg-piotr/nwg-drawer";
+          license = with lib.licenses; [ mit ];
+          mainProgram = "nwg-drawer";
+          platforms = with lib.platforms; linux;
         };
-      });
-
-      devShells = eachSystem (system: {
-        default = pkgsFor.${system}.mkShell {
-          name = "nwg-drawer shell";
-          buildInputs = with pkgsFor.${system}; [
-            go
-            gopls
-            go-tools
-            golangci-lint
-            delve
-            gtk3
-            gtk3-x11
-            pkg-config
-            gobject-introspection
-            cairo
-            glib
-            gtk-layer-shell
-            librsvg
-          ];
-
-          shellHook = ''
-            export PKG_CONFIG_PATH="${pkgsFor.${system}.pkg-config}/lib/pkgconfig:$PKG_CONFIG_PATH"
-            export LD_LIBRARY_PATH="${pkgsFor.${system}.gtk3}/lib:${pkgsFor.${system}.gtk-layer-shell}/lib:$LD_LIBRARY_PATH"
-            export GI_TYPELIB_PATH="${pkgsFor.${system}.gtk3}/lib/girepository-1.0:${pkgsFor.${system}.gtk-layer-shell}/lib/girepository-1.0:$GI_TYPELIB_PATH"
-            export GOPATH="$PWD/.go"
-            export PATH="$GOPATH/bin:$PATH"
-            mkdir -p "$GOPATH"
-          '';
-        };
-      });
-    };
+      };
+    });
+    
+    devShells = eachSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          go
+          cairo
+          gtk-layer-shell
+          gtk3
+          gobject-introspection
+          pkg-config
+          wrapGAppsHook3
+        ];
+      };
+    });
+  };
 }
-
-
